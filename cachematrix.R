be5b987c61279b2d0107dc9dea1998e27679770e
Helio L Mota <helio.mota@gmail.com>
2014-10-25 14:59:21 -0200

    improved comments a bit

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
-## These functions can calculate the inverse of a matrix and cache 
-## the results, so it does not need to be recalculated when re-used.
+## These functions can calculate the inverse of a matrix and cache
+## the results; so it does not need to be recalculated when re-used.
 
-## This function returns a closure that holds both the original 
+## This function returns a closure that holds both the original
 ## matrix passed as argument x and its inverse
 makeCacheMatrix <- function(x = matrix()) {
+    # This variable holds the cached inverse of the matrix
     xi <- NULL
 
     ## Sets the enclosed matrix and resets any cached inverse
@@ -27,60 +28,80 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function expects a closure returned by makeCacheMatrix above 
-## and uses its caching capabilities to return the solution for 
+## This function expects a closure returned by makeCacheMatrix above
+## and uses its caching capabilities to return the solution for
 ## the inverse matrix
 cacheSolve <- function(x, ...) {
-     ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     xi <- x$getSolve()
+
+    # Detects if a cached result was returned
     if(!is.null(xi)) {
       message("getting cached data")
       return(xi)
     }
+
+    ## Gets the matrix...
     data <- x$get()
+
+    ## ... and uses solve to compute the inverse matrix
     xi <- solve(data, ...)
+
+    ## Stores the computed inverse in the cache
     x$setSolve(xi)
+
+    ## Returns the inverse of the matrix
     xi
 }
 
 ## Tests the functions
-message("Test #1: with a 2x2 matrix")
-m <- matrix(c(4, 2, 7, 6), 2, 2)
+run.tests <- function() {
+  message("Test #1: with a 2x2 matrix")
+  m <- matrix(c(4, 2, 7, 6), 2, 2)
 
-# The expected inverse of the above matrix
-mi <- matrix(c(0.6, -0.2, -0.7, 0.4), 2, 2)
+  # The expected inverse of the above matrix
+  mi <- matrix(c(0.6, -0.2, -0.7, 0.4), 2, 2)
 
-cm <- makeCacheMatrix(m)
-print(cm$get())
+  # Initializes the closure with the above matrix
+  cm <- makeCacheMatrix(m)
+  print(cm$get())
 
-xi <- cacheSolve(cm)
-print(cm$getSolve())
+  # Gets the inverse of the above function
+  xi <- cacheSolve(cm)
+  print(cm$getSolve())
 
-## Calculates the inverse again, it should return the cached value
-xi <- cacheSolve(cm)
+  ## Calculates the inverse again, it should return the cached value
+  xi <- cacheSolve(cm)
 
-## Is this really the inverse matrix?
-if(isTRUE(all.equal(xi, mi))) {
-  message("Test #1 OK")
-} else {
-  message("Test #1 FAIL")
-}
+  ## Is this really the inverse matrix?
+  ## This tests compares the inverse matrix returned with the one
+  ## that was expected
+  if(isTRUE(all.equal(xi, mi))) {
+    message("Test #1 OK")
+  } else {
+    message("Test #1 FAIL")
+  }
 
-# Test #2: With a 3x3 matrix
-message("Test #2: with a 3x3 matrix")
-m <- matrix(c(3, 2, 0, 0, 0, 1, 2, -2, 1), 3, 3)
+  # Test #2: With a 3x3 matrix
+  message("Test #2: with a 3x3 matrix")
+  m <- matrix(c(3, 2, 0, 0, 0, 1, 2, -2, 1), 3, 3)
 
-cm <- makeCacheMatrix(m)
-print(cm$get())
+  cm <- makeCacheMatrix(m)
+  print(cm$get())
 
-xi <- cacheSolve(cm)
-print(cm$getSolve())
+  ## Calculates the inverse matrix. It should not use the chache
+  xi <- cacheSolve(cm)
+  print(cm$getSolve())
 
-## Calculates the inverse again, it should return the cached value
-xi <- cacheSolve(cm)
+  ## Once again requests the inverse, this time from the cache
+  xi <- cacheSolve(cm)
 
-if(isTRUE(all.equal(m %*% xi, diag(3)))) {
-  message("Test #2 OK")
-} else {
-  message("Test #2 FAIL")
+  ## Is the returned matrix really the inverse?
+  ## This actually tests if the matrix multiplied by its inverse
+  ## is equal to a 3x3 identity matrix
+  if(isTRUE(all.equal(m %*% xi, diag(3)))) {
+    message("Test #2 OK")
+  } else {
+    message("Test #2 FAIL")
+  }
 }
